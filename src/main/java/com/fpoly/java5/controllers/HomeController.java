d29e0bf8137cd2299913ba7e16af6860d5e168e1
minhdangk19 <156889748+minhdangk19@users.noreply.github.com>
2025-02-25 22:10:28 +0700

    ttt

--- src/main/java/com/fpoly/java5/controllers/HomeController.java
+++ src/main/java/com/fpoly/java5/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package com.fpoly.java5.controllers;
 
+<<<<<<< Updated upstream
 import java.util.List;
 
+=======
+>>>>>>> Stashed changes
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.fpoly.java5.entity.ProductEntity;
+import com.fpoly.java5.jpas.ProductJPA;
+
 import com.fpoly.java5.jpas.ProductJPA;
 
 @Controller
@@ -26,5 +31,8 @@ public class HomeController {
 	public String homePage() {
 		return "/user/index.html";
 	}
+	
+	@Autowired
+	ProductJPA productJPA;
 	
 }
