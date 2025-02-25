d339306dd5e9472fa241c9fc75012633838a1f2b
tuanvtpc <tuanvtpc08813@gmail.com>
2025-02-25 22:13:03 +0700

    zzzz

--- src/main/java/com/fpoly/java5/jpas/ImagesJPA.java
+++ src/main/java/com/fpoly/java5/jpas/ImagesJPA.java
@@ -1,10 +1,12 @@
 package com.fpoly.java5.jpas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.fpoly.java5.entity.ImageEntity;
 
+public interface ImagesJPA extends JpaRepository<ImageEntity, Integer> {
 
-public interface ImagesJPA extends JpaRepository<ImageEntity, Integer>{
-
+	@Query(value = "DELETE FORM Images WHERE product_id=?1", nativeQuery = true)
+	public void deleteByProdId(int id);
 }
--- src/main/java/com/fpoly/java5/services/ProductService.java
+++ src/main/java/com/fpoly/java5/services/ProductService.java
@@ -18,57 +18,56 @@ import com.fpoly.java5.jpas.ProductJPA;
 public class ProductService {
 	@Autowired
 	ImagesJPA imageJPA;
-	
+
 	@Autowired
 	CategoryJPA categoryJPA;
-	
+
 	@Autowired
 	ProductJPA productJPA;
-	
+
 	@Autowired
 	ImageService imageService;
-	
-	
+
 	public boolean saveProduct(ProductBean productBean) {
-        try {
-            ProductEntity product = new ProductEntity();
-            product.setName(productBean.getName());
-            product.setDescription(productBean.getDesc());
-            product.setPrice(productBean.getPrice());
-            product.setQuantity(productBean.getQuantitty());
-            product.setActive(true);
-            product.setDateCreated();
+		try {
+			ProductEntity product = new ProductEntity();
+			product.setName(productBean.getName());
+			product.setDescription(productBean.getDesc());
+			product.setPrice(productBean.getPrice());
+			product.setQuantity(productBean.getQuantitty());
+			product.setActive(true);
+			product.setDateCreated();
 
-            Optional<CategoryEntity> cOptional = categoryJPA.findById(productBean.getCat_id());
-            product.setCategory(cOptional.orElse(null));
+			Optional<CategoryEntity> cOptional = categoryJPA.findById(productBean.getCat_id());
+			product.setCategory(cOptional.orElse(null));
 
-            ProductEntity productSave = productJPA.save(product); // Lưu sản phẩm để có ID
+			ProductEntity productSave = productJPA.save(product); // Lưu sản phẩm để có ID
 
-            // Lưu hình ảnh
-            for (MultipartFile file : productBean.getImages()) {
-                String fileName = imageService.saveImage(file);
-                ImageEntity image = new ImageEntity();
-                image.setImageName(fileName);
-                image.setProduct(productSave);
-                imageJPA.save(image);
-            }
+			// Lưu hình ảnh
+			for (MultipartFile file : productBean.getImages()) {
+				String fileName = imageService.saveImage(file);
+				ImageEntity image = new ImageEntity();
+				image.setImageName(fileName);
+				image.setProduct(productSave);
+				imageJPA.save(image);
+			}
 
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-	
-//	public boolean deleteProduct(int id) {
-//		try {
-//			imageJPA.deledeleteByProdId(id);
-//			productJPA.deleteById(id);
-//		} catch (Exception e) {
-//			return false;
-//		}
-//		return true;
-//	}
+	public boolean deleteProduct(int id) {
+		try {
+			imageJPA.deleteByProdId(id);
+			productJPA.deleteById(id);
+		} catch (Exception e) {
+			return false;
+		}
+
+		return true;
+	}
 }
