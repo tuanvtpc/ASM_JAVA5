39e94850399a4903d8fba02693fdadcba5524271
minhdangk19 <156889748+minhdangk19@users.noreply.github.com>
2025-02-27 00:34:45 +0700

    md commit
    product

--- src/main/java/com/fpoly/java5/jpas/ImagesJPA.java
+++ src/main/java/com/fpoly/java5/jpas/ImagesJPA.java
@@ -1,12 +1,18 @@
 package com.fpoly.java5.jpas;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import com.fpoly.java5.entity.ImageEntity;
 
-public interface ImagesJPA extends JpaRepository<ImageEntity, Integer> {
+import jakarta.transaction.Transactional;
 
-	@Query(value = "DELETE FORM Images WHERE product_id=?1", nativeQuery = true)
-	public void deleteByProdId(int id);
+public interface ImagesJPA extends JpaRepository<ImageEntity, Integer> {
+	
+	
+			@Modifying
+		    @Transactional
+		    @Query(value = "DELETE FROM Images WHERE product_id = ?1", nativeQuery = true)
+		    void deleteByProId(int id);
 }
